ca5c258c682d882728033e45750af5fe73ddf2ec
Diego82 <[djtorrespe@gmail.com]>
2015-02-06 20:31:07 +0100

    -

--- src/ejercicio3/LecturaFichero.java
+++ src/ejercicio3/LecturaFichero.java
@@ -11,13 +11,32 @@ public class LecturaFichero {
 	public static void main(String[] args)throws FileNotFoundException {
 		Scanner in = new Scanner(new File("nombres_mujer.txt"));
 		List<String> lista = new ArrayList<String>();
+		List<String> listaNombresEmpiezanPorA = new ArrayList<String>();
+		List<String> listaNombresNoTerminanEnVocal = new ArrayList<String>();
 		System.out.println("Inicia programa");
 		while(in.hasNext()){
 			lista.add(in.next());
 		}
 		in.close();
 		System.out.println(lista);
+		
+		//Con este bucle añadimos los nombres que empiezan por vocal a nuestra lista
+		for (int i = 0; i < lista.size(); i++) {
+			if (lista.get(i).toLowerCase().matches("^[a].*")) {
+				listaNombresEmpiezanPorA.add(lista.get(i));
+			}
+		}
+		System.out.println(listaNombresEmpiezanPorA);
+		System.out.println(listaNombresEmpiezanPorA.size());
+		
+		//En este bucle añadimos los nombres que no terminan en vocal
+		for (int i = 0; i < lista.size(); i++) {
+			if (!(lista.get(i).toLowerCase().matches(".*[aeiouy]$"))) {
+				listaNombresNoTerminanEnVocal.add(lista.get(i));
+			}
+		}
+		System.out.println(listaNombresNoTerminanEnVocal);
+		System.out.println(listaNombresNoTerminanEnVocal.size());
 
 	}
-
 }
