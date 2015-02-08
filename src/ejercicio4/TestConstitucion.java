bbb6bbaed042b6943a095df8c85a911bcb2965fb
Diego82 <[djtorrespe@gmail.com]>
2015-02-08 23:58:31 +0100

    .

---
+++ src/ejercicio4/TestConstitucion.java
@@ -0,0 +1,70 @@
+package ejercicio4;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+/**
+ * Programa que recoge todas las palabras que hay en la constitucion
+ * @author Diego Jesus Torres Peinado
+ * @version 1.0
+ */
+
+public class TestConstitucion {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		//Declaracion de los ArrayList
+		List<String> listaConstitucion = new ArrayList<String>();
+		//List<String> palabrasConSignosDePuntuacionDelante = new ArrayList<String>();
+		//List<String> palabrasConSignosDePuntuacionDetras = new ArrayList<String>();
+		StringBuilder lista500palabras = new StringBuilder();
+		Scanner in = new Scanner(new File("constitucion.txt"));
+
+		//Aqui recogemos todas las palabras de la constitucion
+		while(in.hasNext()){
+			listaConstitucion.add(in.next());
+		}
+		in.close();
+		
+		//Imprimimos el numero de palabras que tiene la constitucion
+		System.out.println("Numero de palabras que tiene la constitucion: "+listaConstitucion.size());
+		System.out.println("Palabras que tiene la constitucion: "+listaConstitucion);
+		
+		//En este bucle quitamos los signos de puntuacion a las palabras
+		//y las añadimos a la lista
+		for (int i = 0; i < listaConstitucion.size(); i++) {
+			if (listaConstitucion.get(i).matches(".*[.,:;\")-]$")) {				
+				StringBuilder aux = new StringBuilder(listaConstitucion.get(i));
+				listaConstitucion.add(aux.reverse().deleteCharAt(0).reverse().toString());
+			}
+			if (listaConstitucion.get(i).matches("^[.,:;\")-].*")) {
+				StringBuilder aux = new StringBuilder(listaConstitucion.get(i));
+				listaConstitucion.add(aux.deleteCharAt(0).toString());
+			}
+		}
+
+		//En este otro eliminamos las palabras que estaban con los signos de puntuacion
+		//y que previamente hemos eliminado en el bucle anterior
+		for (int i = 0; i < listaConstitucion.size(); i++) {
+			if (listaConstitucion.get(i).matches(".*[.,:;\")-]$")) {				
+				listaConstitucion.remove(i);
+			}
+			if (listaConstitucion.get(i).matches("^[.,:;\")-].*")) {
+				listaConstitucion.remove(i);
+				
+			}
+		}
+
+		System.out.println("Numero de palabras que tiene la constitucion despues del cambio: "+listaConstitucion.size());
+		System.out.println("Palabras que tiene la constitucion: "+listaConstitucion);
+		
+		//Ahora guardamos 500 palabras aleatoriamente en nuestro StringBuilder 
+		for (int i = 0; i < 500; i++) {
+			int aleatorio = (int) (Math.random()*listaConstitucion.size());
+			lista500palabras.append(listaConstitucion.get(aleatorio)+" ");
+		}
+		System.out.println(lista500palabras);
+		
+	}
+}
---
+++ src/ejercicio4/UtilidadesString.java
@@ -0,0 +1,8 @@
+package ejercicio4;
+
+public class UtilidadesString {
+	public static final String[] PREPOSICIONES = { "a", "ante", "bajo", "cabe", "con", "contra", "de", "desde", "en", "entre", "hacia", "hasta", "para", "por", "segun", "sin", "sobre", "tras" };
+	public static final String[] ARTICULOS_DETERMINADOS = { "el", "la", "los", "las" };
+	public static final String[] ARTICULOS_INDETERMINADOS = { "un", "una", "unos", "unas" };
+	
+}
