b7c26ea7910b6706c2f58b09c1e66029680bf765
Diego82 <[djtorrespe@gmail.com]>
2015-02-09 09:29:03 +0100

    .

--- src/ejercicio4/UtilidadesString.java
+++ src/ejercicio4/UtilidadesString.java
@@ -1,7 +1,7 @@
 package ejercicio4;
 
 public class UtilidadesString {
-	public static final String[] PREPOSICIONES = { "a", "ante", "bajo", "cabe", "con", "contra", "de", "desde", "en", "entre", "hacia", "hasta", "para", "por", "segun", "sin", "sobre", "tras" };
+	public static final String[] PREPOSICIONES = { "a", "ante", "bajo", "cabe", "con", "contra", "de", "desde", "en", "entre", "hacia", "hasta", "para", "por", "según", "sin", "sobre", "tras" };
 	public static final String[] ARTICULOS_DETERMINADOS = { "el", "la", "los", "las" };
 	public static final String[] ARTICULOS_INDETERMINADOS = { "un", "una", "unos", "unas" };
 	
